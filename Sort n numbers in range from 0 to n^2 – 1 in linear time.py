8c99d8618f8c1a9f236ebee4bfa45421669e4e9b
bimalvpatel <37544126+bimalvpatel@users.noreply.github.com>
2018-05-05 20:30:10 -0400

    Add files via upload

---
+++ Sort n numbers in range from 0 to n^2 – 1 in linear time.py
@@ -0,0 +1,21 @@
+def radixcount(A):
+    RADIX = len(A)
+
+    tmp ,placement = -1,1
+    for i in range(2):
+        buckets = [[] for _ in range(RADIX)]
+        for j in A:
+            tmp = j/placement
+            buckets[tmp%RADIX].append(j)
+        #print(buckets)
+        t = 0
+        for k in range(len(buckets)):
+            for ele in buckets[k]:
+                A[t] = ele
+                t += 1
+        placement *= RADIX
+    return A
+
+if __name__ == "__main__":
+    A = map(int,raw_input().split())
+    print(radixcount(A))
