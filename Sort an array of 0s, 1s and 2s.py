f2d456abed0605f0415e1339f93404c1fac887f1
bimalvpatel <37544126+bimalvpatel@users.noreply.github.com>
2018-05-05 22:30:09 -0400

    Add files via upload

---
+++ Sort an array of 0s, 1s and 2s.py
@@ -0,0 +1,19 @@
+def sortarr(arr):
+    low = 0
+    high = len(arr)-1
+    mid = 0
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low],arr[mid] = arr[mid],arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[high],arr[mid] = arr[mid],arr[high]
+            high -= 1
+    return arr
+
+if __name__ == "__main__":
+    arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+    print(sortarr(arr))
